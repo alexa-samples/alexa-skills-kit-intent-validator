cd9124bc23c90db1c7442d4b24de0b9f38383a0a
German Viscuso <germanviscuso@gmail.com>
2018-09-04 00:47:11 +0200

    Updated to ASK v2 and localization via i18next

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ The Alexa ASK Intent Validator is designed to help validate your Alexa interacti
 
 The intent validator is a lambda function that simply interprets the received event request from Alexa and states the key pieces of information (intent and slots) back to the user and displays in the card.
 
-To use, simply take the code and deploy as a lambda function (in US East - Virginia). On your skill's configuration's Service Endpoint Type, ensure AWS Lambda ARN is selected, and paste the ARN ID into the form.
+To use, simply take the code and deploy as a lambda function (in valid Alexa AWS regions). On your skill's configuration's Service Endpoint Type, ensure AWS Lambda ARN is selected, and paste the ARN ID into the form.
 
 There are two ways to utilize – issuing a launch request (ie “open”) will have the skill will remain in dialog mode – one can continuously tell it different utterances until you say “exit”.  OneShot is also supported – ie “Ask SkillName to do this utterance” and the Alexa session ends afterwards.
 
-*Note* if utilizing the dialog management features, you may need to include the source of the alexa-sdk in your lambda function.  You can complete this by performing "npm install" and including node_modules with your Lambda source.
+*Note* if utilizing the dialog management features, you may need to include the source of the ask-sdk in your lambda function.  You can complete this by performing "npm install" and including node_modules with your Lambda source.
 
 ## Dialog Management
 
@@ -21,17 +21,21 @@ Dialog Management is supported through the following code snippets:
 const DIALOG_DIRECTIVE_SUPPORT = false;
 
 if (DIALOG_DIRECTIVE_SUPPORT && request.dialogState && request.dialogState !== 'COMPLETED') {
-    this.emit(':delegate');
+    const currentIntent = handlerInput.requestEnvelope.request.intent;
+    return handlerInput.responseBuilder
+        .addDelegateDirective(currentIntent)
+        .getResponse();
 } 
 ~~~
 By setting the above flag to "true", the code will inspect the inbound request for the dialogState and if the dialogState is NOT "COMPLETED", it will always delegate back to the skill for additional prompts.  This works great for intents with confirmation/elicitation built-in, but for other intents this will break.  Thus, we recommend only turning this flag on for specifically testing your dialog directives and turn off when not.
 
 ## Language Support
 
-English, German, French, Italian and Spanish languages are supported.  The function inspects the locale on the request object and appropriately updates the wording.
+English, German, French, Italian and Spanish languages are supported.  The function inspects the locale on the request object via i18next and appropriately updates the wording.
 
 # Updates
 
+4/9/2018 Updated to ASK v2
 Please watch this repo as we will periodically provide updates
 
 # Feedback
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+/* eslint-disable  no-restricted-syntax */
 /* -*- coding: utf-8 -*- */
 
 /*
@@ -11,201 +14,268 @@ language governing permissions and limitations under the License.
 */
 
 /*
-The Alexa ASK Intent Validator is designed for medium-complex Intent Schema validation. Need to quickly 
-try many different combinations of your utterances ON your devices, this is the tool for you.  
+The Alexa ASK Intent Validator is designed for medium-complex Intent Schema validation. If you need 
+to quickly try many different combinations of your utterances ON your devices, this is the tool for you.
 
-Currently supports English, German, French, Italian and Spanish.
+Currently supports all variants of English, German, French, Italian and Spanish.
  **/
 
-'use strict';
-
 // Use the new Alexa SDK
-const Alexa = require('alexa-sdk');
+const Alexa = require('ask-sdk');
+// Use the internationalization library
+const i18n = require('i18next');
+const sprintf = require('i18next-sprintf-postprocessor');
 
 // UPDATEME: Does your skill use Dialog Directives?  If so, update this to true.
 const DIALOG_DIRECTIVE_SUPPORT = true;
 
-// Series of strings for language tokenization
-const LANGUAGE_STRINGS = {
-    'en-US': {
-        'launchRequestResponse': 'Launch Request for dialog mode, the session will remain open until you say, exit',
-        'exit': 'Goodbye.',
-        'received_with': ' received with ',
-        'slot': ' slot. ',
-        'slots': ' slots. ',
-        'still_listening': "I'm still listening,  Please try another intent or say, stop",
-        'received_slots_are': 'Received slots are ',
-        'card_title': 'Reflected Intent'
-    },
-    'en-GB': {
-        'launchRequestResponse': 'Launch Request for dialog mode, the session will remain open until you say, exit',
-        'exit': 'Goodbye.',
-        'received_with': ' received with ',
-        'slot': ' slot. ',
-        'slots': ' slots. ',
-        'still_listening': "I'm still listening,  Please try another intent or say, stop",
-        'received_slots_are': 'Received slots are ',
-        'card_title': 'Reflected Intent'
-    },
-    'en-CA': {
-        'launchRequestResponse': 'Launch Request for dialog mode, the session will remain open until you say, exit',
-        'exit': 'Goodbye.',
-        'received_with': ' received with ',
-        'slot': ' slot. ',
-        'slots': ' slots. ',
-        'still_listening': "I'm still listening,  Please try another intent or say, stop",
-        'received_slots_are': 'Received slots are ',
-        'card_title': 'Reflected Intent'
-    },
-    'en-IN': {
-        'launchRequestResponse': 'Launch Request for dialog mode, the session will remain open until you say, exit',
-        'exit': 'Goodbye.',
-        'received_with': ' received with ',
-        'slot': ' slot. ',
-        'slots': ' slots. ',
-        'still_listening': "I'm still listening,  Please try another intent or say, stop",
-        'received_slots_are': 'Received slots are ',
-        'card_title': 'Reflected Intent'
-    },
-    'de-DE': {
-        'launchRequestResponse': 'Launch Request. Gib den nächsten Befehl oder sage Abbruch',
-        'exit': 'Auf Wiedersehen.',
-        'received_with': ' empfangen mit ',
-        'slot': ' Slot. ',
-        'slots': ' Slots. ',
-        'still_listening': "Ich lausche noch immer. Bitte gebe einen neuen Befehl oder sage Stop.",
-        'received_slots_are': 'Empfangene Slots sind ',
-        'card_title': 'Reflektierte Absicht'
-    },
-    'fr-FR': {
-        'launchRequestResponse': "On passe en mode dialogue, la session restera ouverte jusqu'à ce que vous disiez, arrête",
-        'exit': 'Au revoir.',
-        'received_with': ' reçu avec ',
-        'slot': ' slotte. ',
-        'slots': ' slottes. ',
-        'still_listening': "J'écoute encore,  Veuillez réessayer ou dites, arrête",
-        'received_slots_are': 'Les slottes reçus sont ',
-        'card_title': 'Intent '
-    },
-    'es-ES': {
-        'launchRequestResponse': "Petición de Lanzamiento. Pasando a modo diálogo. La sesión permanecerá abierta hasta que digas para.",
-        'exit': 'Adiós.',
-        'received_with': ' recibido con ',
-        'slot': ' parametro. ',
-        'slots': ' parametros. ',
-        'still_listening': "Sigo aquí. Qué quieres que repita?",
-        'received_slots_are': 'Los parametros recibidos son ',
-        'card_title': 'Intent '
-    },  
-    'it-IT': {
-        'launchRequestResponse': "Richiesta di lancio. Passando alla modalità di dialogo, la sessione rimarrà aperta finché non dirai, esci",
-        'exit': 'Addio.',
-        'received_with': ' ricevuto con ',
-        'slot': ' parametro. ',
-        'slots': ' parametri. ',
-        'still_listening': "Sono ancora. Qui Cosa vuoi che ripeta?",
-        'received_slots_are': 'I parametri ricevuti sono ',
-        'card_title': 'Intent '
-    }  
+
+/* Intent Handlers */
+const LaunchReflectorHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest'
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+    sessionAttributes.dialogSession = true;
+
+    const speechOutput = requestAttributes.t('launchRequestResponse');
+
+    return handlerInput.responseBuilder
+      .speak(speechOutput)
+      .withShouldEndSession(false)
+      .getResponse();
+  }
 }
 
-// The various handlers for interpreting the interaction model from the skill
-const handlers = {
 
-    // Launch request - "open skillName" - keep the session open until the user requests to exit
-    'LaunchRequest': function () {
-        let LANGUAGE = LANGUAGE_STRINGS[this.event.request.locale] ? LANGUAGE_STRINGS[this.event.request.locale] : LANGUAGE_STRINGS['en-US'];
-        this.attributes['dialogSession'] = true;
-        this.emit(':ask', LANGUAGE.launchRequestResponse, LANGUAGE.launchRequestResponse);
+// The main handler - here we simply take the inbound Alexa request, parse out the intent and slots, then return back 
+  // to the user, either as a dialog
+  const UnhandledHandler = {
+    canHandle(handlerInput) {
+      return true;
     },
-    // End the Session
-    'SessionEndedRequest': function () {
-        this.emit('AMAZON.StopIntent');
-    },
-    'AMAZON.StopIntent': function () {
-        let LANGUAGE = LANGUAGE_STRINGS[this.event.request.locale] ? LANGUAGE_STRINGS[this.event.request.locale] : LANGUAGE_STRINGS['en-US'];
-        this.emit(':tell', LANGUAGE.exit);
-    },
-    'AMAZON.ExitIntent': function () {
-        this.emit('AMAZON.StopIntent');
-    },
+    handle(handlerInput) {
+      let request = handlerInput.requestEnvelope.request;
+      const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+      const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+      let intentInfo = parseIntentsAndSlotsFromEvent(request, requestAttributes);
 
-    // The main handler - here we simply take the inbound Alexa request, parse out the intent and slots, then return back 
-    // to the user, either as a dialog
-    'Unhandled': function () {
+      // If dialog directive support is enabled AND it exists and it is not in "completed" status, delegate back to the interaction model
+      if (DIALOG_DIRECTIVE_SUPPORT && request.dialogState && request.dialogState !== 'COMPLETED') {
+        const currentIntent = handlerInput.requestEnvelope.request.intent;
+        return handlerInput.responseBuilder
+          .addDelegateDirective(currentIntent)
+          .getResponse();
+      } else {
+        requestAttributes.intentOutput = intentInfo.cardInfo;
 
-        let request = this.event.request;
-        
-        let intentInfo = parseIntentsAndSlotsFromEvent(request);
-        
-        // If dialog directive support is enabled AND it exists and it is not in "completed" status, delegate back to the interaction model
-        if (DIALOG_DIRECTIVE_SUPPORT && request.dialogState && request.dialogState !== 'COMPLETED') {
-            this.emit(':delegate');
+        // Determine if we are going to end the session or keep it in dialog mode.  When used in dialog mode we use reprompt 
+        // as we are expecting another question to come through.  When used in OneShot mode we say the info and end the session.
+        if (sessionAttributes.dialogSession) {
+          return handlerInput.responseBuilder
+            .speak(intentInfo.response)
+            .reprompt(requestAttributes.t('still_listening'))
+            .withSimpleCard(requestAttributes.t('card_title'), intentInfo.cardInfo)
+            .getResponse();
         } else {
-            this.attributes['intentOutput'] = intentInfo.cardInfo;
-
-            let LANGUAGE = LANGUAGE_STRINGS[request.locale] ? LANGUAGE_STRINGS[request.locale] : LANGUAGE_STRINGS['en-US'];
-            
-            // Determine if we are going to end the session or keep it in dialog mode.  When used in dialog mode we "ask" 
-            // as we are expecting another question to come through.  When used in OneShot mode we "tell" and end the session.
-            if (this.attributes['dialogSession']) {
-                this.emit(':askWithCard', intentInfo.response, LANGUAGE.still_listening, LANGUAGE.card_title, intentInfo.cardInfo);
-            } else {
-                this.emit(':tellWithCard', intentInfo.response, LANGUAGE.card_title, intentInfo.cardInfo);
-            }
+          return handlerInput.responseBuilder
+            .speak(intentInfo.response)
+            .withSimpleCard(requestAttributes.t('card_title'), intentInfo.cardInfo)
+            .getResponse();
         }
+      }
     }
-
-};
-
-/**
+  }
+  
+  /**
  * Parses the collected event info from Alexa into a friendlier TTS response and 
  * creates a card response with Intent/Slot info
  */
-function parseIntentsAndSlotsFromEvent(request) {
-    //Cleanse the request intent name
-    let intentName = request.intent.name.replace(/[^a-zA-Z0-9]/g, " ");
+function parseIntentsAndSlotsFromEvent (request, requestAttributes) {
+  // Cleanse the request intent name
+  let intentName = request.intent.name.replace(/[^a-zA-Z0-9]/g, ' ');
 
-    let numSlots = 0;
-    let slots = request.intent.slots;
+  let numSlots = 0;
+  let slots = request.intent.slots;
 
-    let filledInSlots = {};
+  let filledInSlots = {};
 
-    if (slots) {
-        for (let i in slots) {
-            //Check if there is a value in a given slot
-            if (slots[i].value) {
-                filledInSlots[slots[i].name] = slots[i].value;
-                numSlots++;
-            }
-        }
+  if (slots) {
+    for (let i in slots) {
+      // Check if there is a value in a given slot
+      if (slots[i].value) {
+        filledInSlots[slots[i].name] = slots[i].value;
+        numSlots++;
+      }
     }
+  }
 
-    let LANGUAGE = LANGUAGE_STRINGS[request.locale] ? LANGUAGE_STRINGS[request.locale] : LANGUAGE_STRINGS['en-US'];
-    
-    let responseText = `${intentName} ${LANGUAGE.received_with} ${numSlots} ${LANGUAGE.slots}`;
-    let cardInfo = `${intentName} ${LANGUAGE.received_with} ${numSlots} ${LANGUAGE.slots}`;
+  let responseText = `${intentName} ${requestAttributes.t('received_with')} ${numSlots} ${requestAttributes.t('slots')}`;
+  let cardInfo = `${intentName} ${requestAttributes.t('received_with')} ${numSlots} ${requestAttributes.t('slots')}`;
 
-    if (filledInSlots > 0) {
-        responseText += LANGUAGE.received_slots_are;
-    }
+  if (filledInSlots > 0) {
+    responseText += requestAttributes.t('received_slots_are');
+  }
 
-    for (let slotName in filledInSlots) {
-        responseText += ` ${slotName}, ${filledInSlots[slotName]}. `;
-        cardInfo += `\n${slotName}: ${filledInSlots[slotName]}`;
-    }
+  for (let slotName in filledInSlots) {
+    responseText += ` ${slotName} is ${filledInSlots[slotName]}. `;
+    cardInfo += `\n${slotName}: ${filledInSlots[slotName]}`;
+  }
 
-    return {
-        response: responseText,
-        cardInfo: cardInfo
-    };
+  return {
+    response: responseText,
+    cardInfo: cardInfo
+  }
 }
 
-exports.handler = (event, context) => {
-    console.log("**********  REQUEST **********");
-    console.log(JSON.stringify(event, null, 2));
-    //console.log(JSON.stringify(context, null, 2));
-    const alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
-};
+const ExitHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest'
+      || (handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent'
+      || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'))
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    const speechOutput = requestAttributes.t('exit');
+
+    return handlerInput.responseBuilder
+      .speak(speechOutput)
+      .getResponse();
+  }
+}
+
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    const speakOutput = requestAttributes.t('error');
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(speakOutput)
+      .getResponse();
+  }
+}
+
+
+
+// Interceptor to use i18next based on the locale of the user
+const LocalizationInterceptor = {
+  process(handlerInput) {
+    const localizationClient = i18n.use(sprintf).init({
+      lng: handlerInput.requestEnvelope.request.locale,
+      overloadTranslationOptionHandler: sprintf.overloadTranslationOptionHandler,
+      resources: languageStrings,
+      returnObjects: true
+    })
+
+    const attributes = handlerInput.attributesManager.getRequestAttributes();
+    attributes.t = function (...args) {
+      return localizationClient.t(...args);
+    }
+  }
+}
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    LaunchReflectorHandler,
+    ExitHandler,
+    UnhandledHandler
+  )
+  .addErrorHandlers(
+    ErrorHandler
+  )
+  .addRequestInterceptors(
+    LocalizationInterceptor
+  )
+  .lambda();
+
+
+
+  
+// Series of strings for language tokenization
+const languageStrings = {
+  en: {
+    translation: {
+      launchRequestResponse: 'Launch Request for dialog mode, the session will be kept open until you say, exit',
+      exit: 'Goodbye.',
+      error: "Sorry, I can't understand the command. Please say again.",
+      received_with: ' received with ',
+      slot: ' slot. ',
+      slots: ' slots. ',
+      still_listening: "I'm still listening,  Please try another intent or say, stop",
+      received_slots_are: 'Received slots are ',
+      card_title: 'Reflected Intent'
+    }
+  },
+  'en-GB': {
+    translation: {
+      exit: 'Farewell.'
+    }
+  },
+  de: {
+    translation: {
+      launchRequestResponse: 'Launch Request. Gib den nächsten Befehl oder sage Abbruch',
+      exit: 'Auf Wiedersehen.',
+      error: 'Entschuldigung, ich kann den Befehl nicht verstehen. Bitte noch einmal sagen.',
+      received_with: ' empfangen mit ',
+      slot: ' Slot. ',
+      slots: ' Slots. ',
+      still_listening: 'Ich lausche noch immer. Bitte gebe einen neuen Befehl oder sage Stop.',
+      received_slots_are: 'Empfangene Slots sind ',
+      card_title: 'Reflektierte Absicht'
+    }
+  },
+  fr: {
+    translation: {
+      launchRequestResponse: "On passe en mode dialogue, la session restera ouverte jusqu'à ce que vous disiez, arrête",
+      exit: 'Au revoir.',
+      error: "Désolé, je ne peux pas comprendre la commande. Répétez s'il vous pait.",
+      received_with: ' reçu avec ',
+      slot: ' slotte. ',
+      slots: ' slottes. ',
+      still_listening: "J'écoute encore,  Veuillez réessayer ou dites, arrête",
+      received_slots_are: 'Les slottes reçus sont ',
+      card_title: 'Intent Reflété'
+    }
+  },
+  es: {
+    translation: {
+      launchRequestResponse: 'Petición de Lanzamiento. Pasando a modo diálogo. La sesión permanecerá abierta hasta que digas para.',
+      exit: 'Adiós.',
+      error: 'Lo siento, no puedo entender el comando. Dilo otra vez por favor.',
+      received_with: ' recibido con ',
+      slot: ' parámetro. ',
+      slots: ' parámetros. ',
+      still_listening: 'Sigo aquí. Qué quieres que repita?',
+      received_slots_are: 'Los paraáetros recibidos son ',
+      card_title: 'Intent Reflejado'
+    }
+  },
+  'es-MX': {
+    translation: {
+      exit: 'Nos vemos.'
+    }
+  },
+  it: {
+    translation: {
+      launchRequestResponse: 'Richiesta di lancio. Passando alla modalità di dialogo, la sessione rimarrà aperta finché non dirai, esci',
+      exit: 'Addio.',
+      error: 'Scusa, non riesco a capire il comando. Per favore, ripeti.',
+      received_with: ' ricevuto con ',
+      slot: ' parametro. ',
+      slots: ' parametri. ',
+      still_listening: 'Sono ancora. Qui Cosa vuoi che ripeta?',
+      received_slots_are: 'I parametri ricevuti sono ',
+      card_title: 'Intent Riflesso'
+    }
+  }
+}
